bdcbdf7c5c7bae3481dfac6f8d285053bfa49005
Taichi Kushiro <taichi.kushiro@nextbeat.net>
2020-11-29 16:55:57 +0900

    delte run method for Eff

--- application/src/main/scala/librame/application/service/EffService.scala
+++ application/src/main/scala/librame/application/service/EffService.scala
@@ -5,22 +5,17 @@ import scala.concurrent.{Future, ExecutionContext}
 import org.atnos.eff._
 import org.atnos.eff.either._
 import org.atnos.eff.future._
-import org.atnos.eff.syntax.either._
-import org.atnos.eff.syntax.future._
+import org.atnos.eff.syntax.{either, future}
 import org.atnos.eff.concurrent.Scheduler
 
 
-trait EffService {
+trait EffService extends either with future {
   implicit val scheduler: Scheduler = ExecutorServices.schedulerFromGlobalExecutionContext
 
   type ServiceEither[T]  = Either[error.ServiceErr, T]
   type _serviceEither[R] = ServiceEither |= R
 
   type FutureStack = Fx.fx2[ServiceEither, TimedFuture]
-
-  implicit class EffFutureStackOps[T](eff: Eff[FutureStack, T])(implicit ec: ExecutionContext) {
-    def run: Future[Either[error.ServiceErr, T]] = eff.runEither.runAsync
-  }
 }
 
 object EffService extends EffService
