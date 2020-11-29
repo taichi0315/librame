02879eed54e7e4f584aa3e5974832d4ca6952381
Taichi Kushiro <taichi.kushiro@nextbeat.net>
2020-11-29 16:42:23 +0900

    delete EffExtension by move to EffService

--- application/src/main/scala/librame/application/service/EffService.scala
+++ application/src/main/scala/librame/application/service/EffService.scala
@@ -5,11 +5,18 @@ import scala.concurrent.{Future, ExecutionContext}
 import org.atnos.eff._
 import org.atnos.eff.either._
 import org.atnos.eff.future._
+import org.atnos.eff.syntax.either._
+import org.atnos.eff.syntax.future._
+import org.atnos.eff.concurrent.Scheduler
 
 
 trait EffService {
+  implicit val scheduler: Scheduler = ExecutorServices.schedulerFromGlobalExecutionContext
+
   type ServiceEither[T]  = Either[error.ServiceErr, T]
   type _serviceEither[R] = ServiceEither |= R
+
+  type FutureStack = Fx.fx2[ServiceEither, TimedFuture]
 
   implicit class FutureOps[T](futureValue: Future[T])(implicit ec: ExecutionContext) {
     def toEff[R: _future]: Eff[R, T] = fromFuture(futureValue)
@@ -17,6 +24,10 @@ trait EffService {
 
   implicit class ServiceEitherOps[T](eitherValue: Either[error.ServiceErr, T])(implicit ec: ExecutionContext) {
     def toEff[R: _serviceEither]: Eff[R, T] = fromEither(eitherValue)
+  }
+
+  implicit class EffFutureStackOps[T](eff: Eff[FutureStack, T])(implicit ec: ExecutionContext) {
+    def run: Future[Either[error.ServiceErr, T]] = eff.runEither.runAsync
   }
 }
 
