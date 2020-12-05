992801dbc1e5938cf813cd31b7ff97c94a881999
Taichi Kushiro <taichi.kushiro@nextbeat.net>
2020-12-06 01:12:47 +0900

    add Errors object

--- adapter/src/main/scala/librame/adapter/primary/Errors.scala
+++ adapter/src/main/scala/librame/adapter/primary/Errors.scala
@@ -5,6 +5,8 @@ import play.api.mvc.Results._
 trait Errors {
   def E_NOT_FOUND(str: String)       = NotFound(s"not found $str")
   def E_BAD_REQUEST(str: String)     = BadRequest(s"bad request $str")
+  def E_VALIDATE(str: String)        = BadRequest(s"invalid $str")
+  def E_DUPLICATE(str: String)       = BadRequest(s"duplicate entry $str")
   def E_INTERNAL_SERVER(str: String) = InternalServerError(s"internal server in $str")
   val E_AUTHENTICATION               = Unauthorized("autentication failture")
   val E_AUTHORIZATION                = Unauthorized("authorization failture")
