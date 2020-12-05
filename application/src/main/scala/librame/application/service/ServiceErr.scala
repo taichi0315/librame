544fcd697d7fbef26b238161a563b35056aba1eb
Taichi Kushiro <taichi.kushiro@nextbeat.net>
2020-12-05 23:38:17 +0900

    fixed ServiceErr

--- application/src/main/scala/librame/application/service/ServiceErr.scala
+++ application/src/main/scala/librame/application/service/ServiceErr.scala
@@ -2,26 +2,25 @@ package librame.application.service
 
 package object error {
   
-  sealed trait ServiceErr { val msg: String }
+  sealed trait ServiceErr
 
-  case class ValidateErr(msg: String) extends ServiceErr
+  case class ValidateErr(msg: String)
   object ValidateErr {
-    def apply(str: String) = new ValidateErr(msg = s"invalid $str")
+    def apply(str: String) = new ValidateErr(s"invalid $str")
   }
 
-  case class DuplicateErr(msg: String) extends ServiceErr
+  case class DuplicateErr(msg: String)
   object DuplicateErr {
-    def apply(str: String) = new DuplicateErr(msg = s"use already $str")
+    def apply(str: String) = new DuplicateErr(s"use already $str")
   }
 
-  case class NotFoundErr(msg: String) extends ServiceErr
+  case class NotFoundErr(msg: String)
   object NotFoundErr {
-    def apply(str: String) = new NotFoundErr(msg = s"not found $str")
+    def apply(str: String) = new NotFoundErr(s"not found $str")
   }
 
-
-  case class MailerErr(msg: String) extends ServiceErr
-  object MailerErr {
-    def apply() = new NotFoundErr(msg = "not send mail")
+  case class ServerErr(msg: String)
+  object ServerErr {
+    def apply() = new ServerErr("internal server error")
   }
 }
