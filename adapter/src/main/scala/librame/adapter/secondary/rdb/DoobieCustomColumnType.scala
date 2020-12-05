ddce51a01c6db873a962342e9b8bd7c73e3b98ab
Taichi Kushiro <taichi.kushiro@nextbeat.net>
2020-12-05 22:27:48 +0900

    add doobie custom column mapping of domain.value.Value[UUID]

--- adapter/src/main/scala/librame/adapter/secondary/rdb/DoobieCustomColumnType.scala
+++ adapter/src/main/scala/librame/adapter/secondary/rdb/DoobieCustomColumnType.scala
@@ -1,5 +1,6 @@
 package librame.adapter.secondary.rdb
 
+import java.util.UUID
 import scala.reflect.ClassTag
 
 import doobie.util._
@@ -15,7 +16,16 @@ trait DoobieCustomColumnType {
         .newInstance(str)
         .asInstanceOf[T]
     }
-
   implicit def valueStringPut[T <: Value[String]]: Put[T] =
     Put[String].contramap(_.value)
+
+  implicit def valueUUIDGet[T <: Value[UUID]](implicit tag: ClassTag[T]): Get[T] =
+    Get[String].map { str =>
+      tag.runtimeClass
+        .getConstructor(classOf[UUID])
+        .newInstance(UUID.fromString(str))
+        .asInstanceOf[T]
+    }
+  implicit def valueUUIDPut[T <: Value[UUID]]: Put[T] =
+    Put[String].contramap(_.value.toString)
 }
