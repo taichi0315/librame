0b57f521266fafdc178613f738c2a8f38d9cdc57
Taichi Kushiro <taichi.kushiro@nextbeat.net>
2020-12-06 00:17:27 +0900

    fixed service error

--- application/src/main/scala/librame/application/service/ServiceErr.scala
+++ application/src/main/scala/librame/application/service/ServiceErr.scala
@@ -1,26 +1,5 @@
 package librame.application.service
 
 package object error {
-  
-  sealed trait ServiceErr
-
-  case class ValidateErr(msg: String)
-  object ValidateErr {
-    def apply(str: String) = new ValidateErr(s"invalid $str")
-  }
-
-  case class DuplicateErr(msg: String)
-  object DuplicateErr {
-    def apply(str: String) = new DuplicateErr(s"use already $str")
-  }
-
-  case class NotFoundErr(msg: String)
-  object NotFoundErr {
-    def apply(str: String) = new NotFoundErr(s"not found $str")
-  }
-
-  case class ServerErr(msg: String)
-  object ServerErr {
-    def apply() = new ServerErr("internal server error")
-  }
+  trait ServiceErr
 }
