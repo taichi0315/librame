b7e5bae600439ceee6fd6d4363467fa16cd120db
Taichi Kushiro <taichi.kushiro@nextbeat.net>
2020-12-06 01:23:43 +0900

    fixed small indent

--- adapter/src/main/scala/librame/adapter/primary/Errors.scala
+++ adapter/src/main/scala/librame/adapter/primary/Errors.scala
@@ -3,13 +3,13 @@ package librame.adapter.primary
 import play.api.mvc.Results._
 
 trait Errors {
-  def E_NOT_FOUND(str: String)       = NotFound(s"not found $str")
-  def E_BAD_REQUEST(str: String)     = BadRequest(s"bad request $str")
-  def E_VALIDATE(str: String)        = BadRequest(s"invalid $str")
-  def E_DUPLICATE(str: String)       = BadRequest(s"duplicate entry $str")
+  def E_NOT_FOUND      (str: String) = NotFound(s"not found $str")
+  def E_BAD_REQUEST    (str: String) = BadRequest(s"bad request $str")
+  def E_VALIDATE       (str: String) = BadRequest(s"invalid $str")
+  def E_DUPLICATE      (str: String) = BadRequest(s"duplicate entry $str")
   def E_INTERNAL_SERVER(str: String) = InternalServerError(s"internal server in $str")
-  val E_AUTHENTICATION               = Unauthorized("autentication failture")
-  val E_AUTHORIZATION                = Unauthorized("authorization failture")
+  def E_AUTHENTICATION (str: String) = Unauthorized(s"autentication failture of $str")
+  def E_AUTHORIZATION  (str: String) = Unauthorized(s"authorization failture of $str")
 }
 
 object Errors extends Errors
