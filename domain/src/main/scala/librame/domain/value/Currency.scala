1f938a2f4a987c0702f9037f0a892912547081d6
Taichi Kushiro <k.taichi0315@gmail.com>
2021-01-11 01:53:05 +0900

    fixed small variable naming in constructor

--- domain/src/main/scala/librame/domain/value/Currency.scala
+++ domain/src/main/scala/librame/domain/value/Currency.scala
@@ -3,12 +3,12 @@ package librame.domain.value
 case class Currency(value: String) extends SingleValueObject[String]
 
 object Currency {
-  def apply(rawCurrency: String): Either[Unit, Currency] =
-    Right(rawCurrency)
+  def apply(value: String): Either[Unit, Currency] =
+    Right(value)
       .filterOrElse(isValidate(_), ())
       .map(new Currency(_))
 
-  private def isValidate(rawCurrency: String): Boolean =
-    rawCurrency.length == 3 &&
-    rawCurrency.forall(_.isUpper)
+  private def isValidate(value: String): Boolean =
+    value.length == 3 &&
+    value.forall(_.isUpper)
 }
--- domain/src/main/scala/librame/domain/value/EmailAddress.scala
+++ domain/src/main/scala/librame/domain/value/EmailAddress.scala
@@ -4,13 +4,13 @@ package librame.domain.value
 case class EmailAddress(value: String) extends SingleValueObject[String]
 
 object EmailAddress {
-  def apply(rawEmail: String): Either[Unit, EmailAddress] =
-    Right(rawEmail)
+  def apply(value: String): Either[Unit, EmailAddress] =
+    Right(value)
       .filterOrElse(isValidate(_), ())
       .map(new EmailAddress(_))
 
-  private def isValidate(rawEmail: String): Boolean =
-    rawEmail.matches(EMAIL_REGEXP)
+  private def isValidate(value: String): Boolean =
+    value.matches(EMAIL_REGEXP)
 
   val EMAIL_REGEXP = """(?:[-!#-'*+/-9=?A-Z^-~]+(?:\.[-!#-'*+/-9=?A-Z^-~]+)*|"(?:[!#-\[\]-~]|\\[\x09 -~])*")@[-!#-'*+/-9=?A-Z^-~]+(?:\.[-!#-'*+/-9=?A-Z^-~]+)*"""
 }
