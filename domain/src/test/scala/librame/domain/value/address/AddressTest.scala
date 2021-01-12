7b80b25c2078ac590917355dfdc354ae10d6b909
Taichi Kushiro <k.taichi0315@gmail.com>
2021-01-13 00:55:34 +0900

    add Address value object

---
+++ domain/src/test/scala/librame/domain/value/address/AddressTest.scala
@@ -0,0 +1,19 @@
+package librame.domain.value.address
+
+import org.scalatest.FunSuite
+
+class AddressTest extends FunSuite {
+  test("country code error") {
+    val result = Address("error", "311-3107", "city", "address1", "address2")
+    val except = Left(Address.CountryCodeValidateErr)
+
+    assert(result == except)
+  }
+
+  test("zip code error") {
+    val result = Address("81", "error", "city", "address1", "address2")
+    val except = Left(Address.ZipCodeValidateErr)
+
+    assert(result == except)
+  }
+}
