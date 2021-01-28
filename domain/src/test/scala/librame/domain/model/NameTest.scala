7f9941bbf4effcab6be8fce099f3ae88748ebff6
Taichi Kushiro <k.taichi0315@gmail.com>
2021-01-28 22:28:12 +0900

    fixed assert to assertResult

--- domain/src/test/scala/librame/domain/model/NameTest.scala
+++ domain/src/test/scala/librame/domain/model/NameTest.scala
@@ -3,9 +3,9 @@ package librame.domain.model
 import org.scalatest.FunSuite
 
 class NameTest extends FunSuite {
-  test("fullName") {
-    assert(Name("山田", "太郎").fullName == "山田 太郎")
-    assert(Name("Yamada", "Taro").fullName == "Yamada Taro")
-    assert(Name("YAMADA", "Taro").fullName == "YAMADA Taro")
+  test("fullName value") {
+    assertResult("山田 太郎")(Name("山田", "太郎").fullName)
+    assertResult("Yamada Taro")(Name("Yamada", "Taro").fullName)
+    assertResult("YAMADA Taro")(Name("YAMADA", "Taro").fullName)
   }
 }
--- domain/src/test/scala/librame/domain/model/SingleValueObjectTest.scala
+++ domain/src/test/scala/librame/domain/model/SingleValueObjectTest.scala
@@ -4,20 +4,28 @@ import org.scalatest.FunSuite
 
 class SingleValueObjectTest extends FunSuite {
   
-  test("同じ型で同じ値を持っている場合等しい") {
-    case class A(value: String) extends SingleValueObject[String]
+  case class A(value: String) extends SingleValueObject[String]
 
-    val a1 = A("test")
-    val a2 = A("test")
-
-    assert(a1 == a2)
+  test("String: 同じ値を持っている場合等しい") {
+    assertResult(A("test"))(A("test"))
   }
 
-  test("同じ型で違う値を持っている場合等しくない") {
-    case class A(value: String) extends SingleValueObject[String]
-
+  test("String: 違う値を持っている場合等しくない") {
     val a1 = A("test1")
     val a2 = A("test2")
+
+    assert(a1 != a2)
+  }
+
+  case class B(value: Int) extends SingleValueObject[Int]
+
+  test("Int: 同じ値を持っている場合等しい") {
+    assertResult(B(1))(B(1))
+  }
+
+  test("Int: 違う値を持っている場合等しくない") {
+    val a1 = B(1)
+    val a2 = B(2)
 
     assert(a1 != a2)
   }
