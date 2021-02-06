48009996c338f5a23c5d50121de9b4bb499c29e4
Taichi Kushiro <k.taichi0315@gmail.com>
2021-02-06 15:36:30 +0900

    fixed MappedColumnType to SetParamter and GetResult in SlickCustomColumnType

--- secondary-adapter/src/main/scala/librame/secondary/adapter/rdb/SlickCustomColumnType.scala
+++ secondary-adapter/src/main/scala/librame/secondary/adapter/rdb/SlickCustomColumnType.scala
@@ -9,36 +9,17 @@ import slick.jdbc.{SetParameter, GetResult}
 import librame.domain.model.SingleValueObject
 
 
-trait SlickCustomColumnType extends SlickDatabaseConfig {
-  import profile.api._
-
-  implicit def valueStringMapped[T <: SingleValueObject[String]](implicit tag: ClassTag[T]) =
-    MappedColumnType.base[T, String](
-      vo  => vo.value,
-      str => tag.runtimeClass
-        .getConstructor(classOf[String])
-        .newInstance(str)
-        .asInstanceOf[T]
-    )
-
-  implicit def valueUUIDMapped[T <: SingleValueObject[UUID]](implicit tag: ClassTag[T]) =
-    MappedColumnType.base[T, String](
-      vo  => vo.value.toString,
-      str => tag.runtimeClass
-        .getConstructor(classOf[UUID])
-        .newInstance(UUID.fromString(str))
-        .asInstanceOf[T]
-    )
+trait SlickCustomColumnType {
 
   implicit val valueStringSet = SetParameter[SingleValueObject[String]] {
     case (vo, params) => params.setString(vo.value)
   }
 
-  implicit def valueStringGet[T <: SingleValueObject[String]](implicit tag: ClassTag[T]) =
-    GetResult[SingleValueObject[String]] {
-      result => tag.runtimeClass
+  implicit def valueStringGet[T <: SingleValueObject[String]](implicit tag: ClassTag[T]): GetResult[T] =
+    GetResult[T] {
+      r => tag.runtimeClass
         .getConstructor(classOf[String])
-        .newInstance(result.nextString)
+        .newInstance(r.<<[String])
         .asInstanceOf[T]
     }
 
@@ -46,11 +27,11 @@ trait SlickCustomColumnType extends SlickDatabaseConfig {
     case (vo, params) => params.setString(vo.value.toString)
   }
 
-  implicit def valueUUIDGet[T <: SingleValueObject[UUID]](implicit tag: ClassTag[T]) =
-    GetResult[SingleValueObject[UUID]] {
-      result => tag.runtimeClass
+  implicit def valueUUIDGet[T <: SingleValueObject[UUID]](implicit tag: ClassTag[T]): GetResult[T] =
+    GetResult[T] {
+      r => tag.runtimeClass
         .getConstructor(classOf[UUID])
-        .newInstance(UUID.fromString(result.nextString))
+        .newInstance(UUID.fromString(r.<<[String]))
         .asInstanceOf[T]
     }
 }
