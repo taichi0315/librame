2b175ab0662107ae5b9a2b46b832606d5264ae35
Taichi Kushiro <k.taichi0315@gmail.com>
2021-02-08 00:15:28 +0900

    add enum to custom column mapping in doobie

--- secondary-adapter/src/main/scala/librame/secondary/adapter/rdb/DoobieCustomColumnType.scala
+++ secondary-adapter/src/main/scala/librame/secondary/adapter/rdb/DoobieCustomColumnType.scala
@@ -8,9 +8,11 @@ import doobie.util._
 
 import librame.domain.model._
 import librame.domain.model.market._
+import librame.domain.model.util._
 
 trait DoobieCustomColumnType {
 
+  /** single value string */
   implicit def valueStringGet[T <: SingleValueObject[String]](implicit tag: ClassTag[T]): Get[T] =
     Get[String].map { str =>
       tag.runtimeClass
@@ -21,6 +23,7 @@ trait DoobieCustomColumnType {
   implicit def valueStringPut[T <: SingleValueObject[String]]: Put[T] =
     Put[String].contramap(_.value)
 
+  /** single value uuid */
   implicit def valueUUIDGet[T <: SingleValueObject[UUID]](implicit tag: ClassTag[T]): Get[T] =
     Get[String].map { str =>
       tag.runtimeClass
@@ -31,9 +34,32 @@ trait DoobieCustomColumnType {
   implicit def valueUUIDPut[T <: SingleValueObject[UUID]]: Put[T] =
     Put[String].contramap(_.value.toString)
 
+  /** market value object */
   implicit val moneyWrite: Write[Money] =
     Write[(BigDecimal, String)].contramap(p => (p.value, p.currency.code))
 
   implicit val priceWrite: Write[Price] =
     Write[(BigDecimal, String)].contramap(p => (p.money.value, p.money.currency.code))
+
+  /** util string enum object */
+  implicit def stringEnumGet[T <: StringEnum](implicit tag: ClassTag[T]): Get[T] =
+    Get[String].map { str =>
+      tag.runtimeClass
+        .getConstructor(classOf[String])
+        .newInstance(str)
+        .asInstanceOf[T]
+  }
+  implicit def stringEnumPut[T <: StringEnum]: Put[T] =
+    Put[String].contramap(_.code)
+
+  /** util int enum object */
+  implicit def intEnumGet[T <: IntEnum](implicit tag: ClassTag[T]): Get[T] =
+    Get[Int].map { int =>
+      tag.runtimeClass
+        .getConstructor(classOf[Int])
+        .newInstance(int)
+        .asInstanceOf[T]
+  }
+  implicit def intEnumPut[T <: IntEnum]: Put[T] =
+    Put[Int].contramap(_.code)
 }
